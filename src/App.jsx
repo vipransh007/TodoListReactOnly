9ac41ef08c7e026fab09d90fea56d4549085f620
Vipransh <vipransh231527@gmail.com>
2025-05-09 04:08:08 +0530

    changed Form and context Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { use, useEffect, useState } from 'react'
+import {Todoprovider} from "./contexts/TodoContext" 
+
 import './App.css'
+import { compile } from 'tailwindcss';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todos, setTodos] = useState([]);
+  const addTodo = (todo) =>{
+    setTodos((prev) => [{id:Date.now(), ...todo}, ...prev])
+  }
+  const updatedTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? 
+   {...prevTodo, compeleted: !prevTodo.compeleted} : prevTodo))
+  }
+
+
+useEffect(() => {
+  const todos =  JSON.parse(localStorage.getItem("todos"))
+  
+  if(todos && todos.length > 0){
+    setTodos(todos);
+  }
+}, [])
+
+useEffect(() => {
+  localStorage.setItem("todos", JSON.stringify(todos))
+}, [todos])
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Todoprovider value={{
+      todos, addTodo,updatedTodo,deleteTodo,toggleComplete
+    }}>
+    <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
+    </Todoprovider>
   )
 }
 
